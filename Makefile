5f18d370c02f7b48dc9cb0370753e897ee7eb96d
papawolf42 <papawolf@kakao.com>
2022-02-17 23:13:19 +0900

    Implement plus

---
+++ Makefile
@@ -0,0 +1,2 @@
+all :
+	gcc main.c plus.c -o cal
--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	printf("Hello I'm Calculator");
+int ft_plus(int left, int right);
+
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int left;
+	int right;
+
+	if (argc != 4)
+	{
+		printf("Usage : \"./cal a + b\"\n");
+		return (1);
+	}
+
+	// from here, right fommat is received
+	left = atoi(argv[1]);
+	right = atoi(argv[3]);
+
+	if (argv[2][0] == '+')
+		result = ft_plus(left, right);
+	//else if (argv[2][0] == '*')
+		//result = ft_multi(left, right);
+
+	printf("result: %d\n", result);
+
+	return (0);
 }
